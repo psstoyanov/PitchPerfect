608b816c5fd13e360862bb045b234add8b28257a
PakoSt <kokotekilata@gmail.com>
2018-02-21 14:46:27 +0000

    L5_E11: Add tags to playback buttons and Configure Playback actions

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -18,9 +18,7 @@ class PlaySoundsViewController: UIViewController {
     var audioPlayerNode: AVAudioPlayerNode!
     var stopTimer: Timer!
     
-    enum ButtonType: Int {
-        case slow = 0, fast, chipmunk, vader, echo, reverb
-    }
+    enum ButtonType: Int { case slow = 0, fast, chipmunk, vader, echo, reverb }
     
     // Outlets for the playback buttons.
     
@@ -49,11 +47,27 @@ class PlaySoundsViewController: UIViewController {
     //Playback actions
     
     @IBAction func playSoundForButton(_ sender: UIButton) {
-        print("Play Sound Button Pressed")
+        switch(ButtonType(rawValue: sender.tag)!) {
+           case .slow:
+               playSound(rate: 0.5)
+           case .fast:
+               playSound(rate: 1.5)
+           case .chipmunk:
+               playSound(pitch: 1000)
+           case .vader:
+               playSound(pitch: -1000)
+           case .echo:
+               playSound(echo: true)
+           case .reverb:
+               playSound(reverb: true)
+        }
+        configureUI(.playing);
+        
     }
     
+    
     @IBAction func stopButtonPressed(_ sender: AnyObject) {
-        print("Stop Audio Button Pressed")
+        stopAudio();
     }
 
 }
