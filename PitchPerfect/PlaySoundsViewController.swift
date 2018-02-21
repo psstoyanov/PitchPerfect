a5c271f46fa17529e9bdfb114e47005818411fbb
PakoSt <kokotekilata@gmail.com>
2018-02-21 13:00:52 +0000

    L5_E8: Connect the Playback buttons to IBOutlets and  IBActions

--- PitchPerfect/PlaySoundsViewController.swift
+++ PitchPerfect/PlaySoundsViewController.swift
@@ -11,6 +11,17 @@ import UIKit
 class PlaySoundsViewController: UIViewController {
     
     var recordedAudioURL: URL!
+    
+    // Outlets for the playback buttons.
+    
+    @IBOutlet weak var snailButton: UIButton!
+    @IBOutlet weak var chipmunkButton: UIButton!
+    @IBOutlet weak var rabbitButton: UIButton!
+    @IBOutlet weak var vaderButton: UIButton!
+    @IBOutlet weak var echoButton: UIButton!
+    @IBOutlet weak var reverbButton: UIButton!
+    @IBOutlet weak var stopButton: UIButton!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +32,16 @@ class PlaySoundsViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    //Playback actions
+    
+    @IBAction func playSoundForButton(_ sender: UIButton) {
+        print("Play Sound Button Pressed")
+    }
+    
+    @IBAction func stopButtonPressed(_ sender: AnyObject) {
+        print("Stop Audio Button Pressed")
     }
     
 
